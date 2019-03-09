ec7d35f05483cadb58a0d4cfdeeefe067c0d89ea
Jerrik Arango <jerrik.arango@turner.com>
2019-03-09 14:34:16 -0500

    cleanup

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,83 +3,100 @@
 namespace Drupal\tcm_api\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Response;
 use GuzzleHttp\Client;
-use Drupal\Core\Entity\ContentEntityNullStorage;
-use \Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+/**
+ * Controller class.
+ */
 class MoviesController extends ControllerBase {
 
-	private $endpoint = "http://api.tcm.com/tcmws/v1/vod/latest/6.json";
+  private $endpoint = "http://api.tcm.com/tcmws/v1/vod/latest/6.json";
 
-	public function moviesPage() {
+  /**
+   * All movies page.
+   */
+  public function moviesPage() {
 
-		$movies = $this->getMovies();
-		// Render Array
-		$build = array(
-			'#theme' => 'movies',
-			'#type' => 'markup',
-			'#attached' => array(
-				'library' => array(
-					'tcm_api/movies',
-				),
-			),
-			'#movies' => $movies,
-		);
+    $movies = $this->getMovies();
+    // Render Array.
+    $build = [
+      '#theme' => 'movies',
+      '#type' => 'markup',
+      '#attached' => [
+        'library' => [
+          'tcm_api/movies',
+        ],
+      ],
+      '#movies' => $movies,
+    ];
 
-		return $build;
-	}
+    return $build;
+  }
 
-	public function endpoint() {
+  /**
+   * API enpoint.
+   */
+  public function endpoint() {
     return $this->endpoint;
-	}
+  }
 
+  /**
+   * Retrieves Movie ID's and then retrieves each movie as an entity.
+   */
   public function getMovies() {
     $client = new Client();
     $response = $client->request('GET', $this->endpoint());
-		$data = json_decode($response->getBody(), TRUE);
-		$movieIds = $this->getMovieIds($data);
+    $data = json_decode($response->getBody(), TRUE);
+    $movieIds = $this->getMovieIds($data);
 
-		$movie_entities = array();
+    $movie_entities = [];
 
-		foreach ($movieIds as $id) {
-			$movie_entities = $this->getMovieEntity($id);
-		}
+    foreach ($movieIds as $id) {
+      $movie_entities = $this->getMovieEntity($id);
+    }
 
-		return $movie_entities;
-	}
+    return $movie_entities;
+  }
 
-	public function getMovieIds($data) {
-		$ids = array();
+  /**
+   * Retrieves Movie title ID's.
+   */
+  public function getMovieIds($data) {
+    $ids = [];
     // Find our movie based on titleId.
     $results = $data['tcm']['titles'];
     foreach ($results as $result) {
-			$ids[] = $result['titleId'];
-		}
+      $ids[] = $result['titleId'];
+    }
 
     return $ids;
   }
 
-	public function getMovieEntity($titleId){
-		$movie_class = 'Drupal\tcm_api\Entity\Movie';
-		$entity = NULL;
+  /**
+   * Loads each movie as an entity.
+   */
+  public function getMovieEntity($titleId) {
+    $movie_class = 'Drupal\tcm_api\Entity\Movie';
+    $entity = NULL;
 
-		if ($titleId) {
-			// Get a storage object.
-			$movie_storage = \Drupal::entityTypeManager()->getStorage('movie');
-			$entity = $movie_storage->load($titleId);
+    if ($titleId) {
+      // Get a storage object.
+      $movie_storage = \Drupal::entityTypeManager()->getStorage('movie');
+      $entity = $movie_storage->load($titleId);
 
-			// If Movie could not be loaded, throw 404.
-			if (!($entity instanceof $movie_class)) {
-				throw new NotFoundHttpException();
-			}
-		} else {
-			// If no Title ID was found, throw 404.
-			throw new NotFoundHttpException();
-		}
+      // If Movie could not be loaded, throw 404.
+      if (!($entity instanceof $movie_class)) {
+        throw new NotFoundHttpException();
+      }
+    }
+    else {
+      // If no Title ID was found, throw 404.
+      throw new NotFoundHttpException();
+    }
 
-		return $entity;
-	}
+    return $entity;
+  }
 
 
 }
