1753b1c4323ba24afd185c40e9b80062fdcdcbb8
Jerrik Arango <jerrik.arango@turner.com>
2019-03-11 14:19:18 -0400

    csv

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -153,7 +153,7 @@ class MoviesController extends ControllerBase {
   }
 
   public function csvFile() {
-    $file = "/sites/all/modules/custom/tcm_api/CodeChallengeData.csv";
+    $file = drupal_get_path('module', 'tcm_api') + "CodeChallengeData.csv";
     $delimiter = ',';
 
     if (($handle = fopen($file, 'r')) === false) {
