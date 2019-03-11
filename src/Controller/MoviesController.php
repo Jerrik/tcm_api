d48cc459b80c6c6f95f4422890c7a34fe3024d90
Jerrik Arango <jerrik.arango@turner.com>
2019-03-11 14:13:05 -0400

    csv

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -153,7 +153,7 @@ class MoviesController extends ControllerBase {
   }
 
   public function csvFile() {
-    $file = "./../../CodeChallengeData.csv";
+    $file = "./CodeChallengeData.csv";
     $delimiter = ',';
 
     if (($handle = fopen($file, 'r')) === false) {
