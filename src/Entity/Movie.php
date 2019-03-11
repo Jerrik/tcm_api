f9a44824f52d4e12a1d036f19097cb85b70246b6
Jerrik Arango <jerrik.arango@turner.com>
2019-03-11 12:51:15 -0400

    runtime

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -43,19 +43,9 @@ class Movie extends ContentEntityBase {
       '#image' => $entity['image'],
       '#description' => $entity['description'],
       '#year' => $entity['year'],
-      '#runtime' => $this->convertToHoursMins($entity['runtime']),
+      '#runtime' => $entity['runtime'],
       '#rating' => $entity['rating'],
     ];
-  }
-
-  public function convertToHoursMins($minutes) {
-    $format = '%02d:%02d';
-    if ($minutes < 1) {
-      return;
-    }
-    $hours = floor($minutes / 60);
-    $minutes = ($minutes % 60);
-    return sprintf($format, $hours, $minutes);
   }
 
 }
--- src/Entity/MovieStorage.php
+++ src/Entity/MovieStorage.php
@@ -62,9 +62,19 @@ class MovieStorage extends ContentEntityNullStorage {
       'description' => $movie['description'],
       'image' => $movie['imageProfiles'][0]['url'],
       'year' => $movie['releaseYear'],
-      'runtime' => $movie['runtimeMinutes'],
+      'runtime' => $this->convertToHoursMins($movie['runtimeMinutes']),
       'rating' => $movie['tvRating'],
     ];
+  }
+
+  public function convertToHoursMins($minutes) {
+    $format = '%02d:%02d';
+    if ($minutes < 1) {
+      return;
+    }
+    $hours = floor($minutes / 60);
+    $minutes = ($minutes % 60);
+    return sprintf($format, $hours, $minutes);
   }
 
 }
